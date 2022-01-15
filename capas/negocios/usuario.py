8d4f952752927c346fe2c745bf658f064f3336af
Alberto Soriano <rockalber212@gmail.com>
2022-01-15 17:15:32 -0500

    Refinando objeto usuario a nuevas tablas

--- capas/negocios/usuario.py
+++ capas/negocios/usuario.py
@@ -6,18 +6,17 @@ from capas.datos.database import DataBase
 db = DataBase()
 
 class Usuario():
-    def __init__(self, nombre: str, usuario: str, email: str, contrasenia: str, id: str = None) -> None:
-        self.id = id
+    def __init__(self, cedula: str, nombre: str, email: str, contrasenia: str) -> None:
+        self.cedula = cedula
         self.nombre = nombre
-        self.usuario = usuario
         self.email = email
         self.contrasenia = self.sha1(contrasenia)
 
-    def __str__():
-        return Usuario
+    def __str__(self):
+        return 'Usuario(Cedula: {u.cedula}, Nombre: {u.nombre}, Email: {u.email}, Contraseña: {u.contrasenia})'.format(u = self)
 
     def registrarUsuario(self) -> int:
-        res = db.insert('usuario', nombre = self.nombre, usuario = self.usuario,
+        res = db.insert('usuario', cedula = self.cedula, nombre = self.nombre,
                         email = self.email, contrasenia = self.contrasenia)
         match res:
             case 1:
@@ -42,7 +41,7 @@ class Usuario():
         
         if type(datos) is tuple:
             if len(datos) == 1:
-                usuario = Usuario(datos[0][1], datos[0][2], datos[0][3], datos[0][4], datos[0][0])
+                usuario = Usuario(datos[0][0], datos[0][1], datos[0][2], datos[0][3])
                 return usuario
             else:
                 #No existe el usuario
