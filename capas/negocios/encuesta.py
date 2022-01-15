ae4e6e98291960d93de0007bae22b5c6e6dc7724
Alberto Soriano <rockalber212@gmail.com>
2022-01-15 18:30:55 -0500

    Creando encuestas.py y clases de encuesta, pregunta y opcion

--- capas/negocios/encuesta.py
+++ capas/negocios/encuesta.py
@@ -1,6 +1,47 @@
-import hashlib
-from varname import nameof
-
+from lib2to3.pgen2.token import OP
 from capas.datos.database import DataBase
 
 db = DataBase()
+
+class Pregunta():
+    def __init__(self, pos_pregunta: int, enunciado: str) -> None:
+        self.pos_pregunta = pos_pregunta
+        self.enunciado = enunciado
+
+class Abierta(Pregunta):
+    def __init__(self, enunciado: str, pos_pregunta: int = 0) -> None:
+        super().__init__(pos_pregunta, enunciado)
+
+class Opcion():
+    def __init__(self, enunciado: str, pos_opcion: int = 0) -> None:
+        self.pos_opcion = pos_opcion
+        self.enuciado = enunciado
+
+class Cerrada(Pregunta):
+    def __init__(self, enunciado: str, seleccionar_varias: bool, pos_pregunta: int = 0) -> None:
+        super().__init__(pos_pregunta, enunciado)
+        self.seleccionar_varias = seleccionar_varias
+        self.opciones = []
+
+    def agregar_opcion(self, opcion: Opcion):
+        if len(self.opciones) == 0:
+            opcion.pos_opcion = 1
+        else:
+            opcion.pos_opcion = self.opciones[-1].pos_opcion + 1
+        
+        self.opciones.append(opcion)
+
+class Encuesta():
+    def __init__(self, titulo: str, fecha_creacion: str = '', id_encuesta: int = 0) -> None:
+        self.id_encuesta = id_encuesta
+        self.fecha_creacion = fecha_creacion
+        self.titulo = titulo
+        self.preguntas = []
+
+    def agregar_pregunta(self, pregunta: Pregunta):
+        if len(self.preguntas) == 0:
+            pregunta.pos_pregunta = 1
+        else:
+            pregunta.pos_pregunta = self.preguntas[-1].pos_pregunta + 1
+        
+        self.preguntas.append(pregunta)
--- capas/negocios/usuario.py
+++ capas/negocios/usuario.py
@@ -1,5 +1,4 @@
 import hashlib
-from varname import nameof
 
 from capas.datos.database import DataBase
 
